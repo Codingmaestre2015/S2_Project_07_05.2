542a253769ce4a500c7c1c1b45399ca83731fc60
Patrick Clawson <patrick.clawson@west-mec.org>
2018-03-15 13:47:26 -0700

    Initial Code Commit
    All student files

---
+++ sub_cart_txt.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+   Author: 
+   Date:   
+
+   Filename: sub_cart.js
+
+
+   Functions List:
+   setupCart() 
+      Sets up the event handlers for the Add to Order buttons on the web page.
+      
+   addItem(e)
+      Adds the food item associated with the Add to Order button to the shopping
+      cart, keeping track of the number of items of each product ordered by 
+      the customer.
+
+*/
